7742be78e58dcacc72b549ad845be9374d0a1a66
MoyTW <moytravis@gmail.com>
2018-03-05 19:38:18 -0800

    Shim DungeonState

---
+++ Executor/Dungeon/DungeonState.cs
@@ -0,0 +1,24 @@
+﻿using Executor.Components;
+using Executor.Dungeon;
+using Executor.Enums;
+using Executor.GameEvents;
+using Executor.GameQueries;
+
+using RogueSharp;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+
+namespace Executor.Dungeon
+{
+    public class DungeonState
+    {
+        private List<FloorState> floors;
+
+        public Entity Player { get; }
+
+    }
+}
